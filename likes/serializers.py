51895a7d874162261306881583d24bcbf1e83f62
Istem Karakas <istem.karakas@gmail.com>
2025-03-27 15:17:11 +0000

    Implement likes functionality with ListCreate and RetrieveDestroy generic views

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,6 +1,6 @@
 from django.db import IntegrityError
 from rest_framework import serializers
-from .models import Like
+from likes.models import Like
 
 class LikeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
@@ -9,7 +9,7 @@ class LikeSerializer(serializers.ModelSerializer):
         model = Like
         fields = ['id', 'created_at', 'owner', 'post']
     
-    def create(self, validate_data):
+    def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
