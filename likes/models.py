455c415ecfce46cfd7765c75109dedd46c9bf3bd
Istem Karakas <istem.karakas@gmail.com>
2025-03-27 14:05:16 +0000

    Fix duplicate likes by handling IntegrityError in LikeSerializer

--- likes/models.py
+++ likes/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
 
-
-class Comment(models.Model):
+class Like(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='likes', on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    content = models.TextField()
 
     class Meta:
         ordering = ['-created_at']
+        unique_together = ['owner', 'post']
 
     def __str__(self):
-        return self.content
+        return f'{self.owner} {self.post}'
