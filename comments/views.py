b2c1bd364a65f3f31cf025e8e507a0c833edd609
Istem Karakas <istem.karakas@gmail.com>
2025-03-29 12:04:42 +0000

    Added comment filter to retrieve all comments associated with a given post

--- comments/views.py
+++ comments/views.py
@@ -1,17 +1,28 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from bonsaiHive_P5.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
+
 class CommentList(generics.ListCreateAPIView):
+    """
+    List comments or create a comment if logged in.
+    """
     serializer_class = CommentSerializer
     permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_field = ['post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment, or update or delete it by id if own it.
+    """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
