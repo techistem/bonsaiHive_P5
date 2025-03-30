c3e1a19f41779e3254b88ed4d2fa46ae5b06ffd2
Istem Karakas <istem.karakas@gmail.com>
2025-03-30 16:32:55 +0100

    Update ALLOWED_HOSTS for Heroku deployment

--- bonsaiHive_P5/settings.py
+++ bonsaiHive_P5/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://bonsaihive-e8abb81b9040.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bonsaihive-e8abb81b9040.herokuapp.com']
 
 
 # Application definition
