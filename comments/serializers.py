248b1ed4172fcd7b6fa36b12a55a110a310026d3
Istem Karakas <istem.karakas@gmail.com>
2025-03-30 14:30:39 +0100

    Explain JWT authentication and its differences from session-based authentication

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,15 +1,25 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
+
 
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_created_on(self, obj):
+        return naturaltime(obj.created_on)
+
+    def get_updated_on(self, obj):
+        return naturaltime(obj.updated_on)
 
     class Meta:
         model = Comment
@@ -17,6 +27,7 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'post', 'created_at', 'updated_at', 'content'
         ]
+
+
 class CommentDetailSerializer(CommentSerializer):
     post = serializers.ReadOnlyField(source='post.id')
-
