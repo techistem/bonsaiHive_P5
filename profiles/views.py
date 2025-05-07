89853bcaea65c1d505bea3a0c0c5544a46e0c1e6
Istem Karakas <istem.karakas@gmail.com>
2025-05-07 16:23:00 +0100

    syntax error fixed

--- profiles/views.py
+++ profiles/views.py
@@ -13,7 +13,7 @@ class ProfileList(generics.ListAPIView):
     """
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
-        follower_count=Count('owner__followed', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
