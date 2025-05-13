7691cd29580a8286e54f355d2a0c378efc4b7a27
Istem Karakas <istem.karakas@gmail.com>
2025-05-13 09:25:19 +0100

    Configure Cloudinary storage for media and default static files handling

--- bonsaiHive_P5/settings.py
+++ bonsaiHive_P5/settings.py
@@ -199,3 +199,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STORAGES = {
+    "default": {
+       "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage", 
+       },
+    "staticfiles": {
+       "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage", 
+       },
+}
