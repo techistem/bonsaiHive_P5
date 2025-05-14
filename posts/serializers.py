718a4e84dd8aca53ba0883d1dc824aa51124ca3d
Istem Karakas <istem.karakas@gmail.com>
2025-05-14 05:38:37 +0100

    Refactor Post serializer to improve readability and structure

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 from posts.models import Post
 from likes.models import Like
 
+
 class PostSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the post model
+    Adds three extra fields when returning a list of Comment instances
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
@@ -10,34 +15,43 @@ class PostSerializer(serializers.ModelSerializer):
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
-    image = serializers.ImageField()
- 
+
     def validate_image(self, value):
-        if value.size > 1024 * 1024 * 2:
+        """
+        Validates the image file size and height
+        and sends a message if file is too large to upload
+        """
+        if value.size > 5 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 5MB!')
+        if value.image.height > 4096:
             raise serializers.ValidationError(
-                'Image size larger than 2MB!'
+                'Image height larger than 4096px!'
             )
         if value.image.width > 4096:
             raise serializers.ValidationError(
                 'Image width larger than 4096px!'
             )
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px!'
-            )
         return value
 
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
-    
     def get_like_id(self, obj):
+        """
+        returns the like id if it exists for the post
+        """
         user = self.context['request'].user
         if user.is_authenticated:
             like = Like.objects.filter(
                 owner=user, post=obj
             ).first()
             return like.id if like else None
+        return None
+
+    def get_is_owner(self, obj):
+        """
+        returns true if the user making rthe request is
+        the owner of the object
+        """
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Post
