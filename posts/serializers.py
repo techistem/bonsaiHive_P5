6fde232d1744f7be399b945822fd7bc82f051f0d
Istem Karakas <istem.karakas@gmail.com>
2025-05-14 04:52:25 +0100

    Add missing return statement to Post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -25,6 +25,7 @@ class PostSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 'Image height larger than 4096px!'
             )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
