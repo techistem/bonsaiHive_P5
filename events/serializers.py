ea5c1886ff8d075266a221e99d3fa2cc72d53187
Istem Karakas <istem.karakas@gmail.com>
2025-09-12 10:00:03 +0100

    Add validation to EventSerializer for start and end time

--- events/serializers.py
+++ events/serializers.py
@@ -5,6 +5,15 @@ from .models import Event
 class EventSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
+    def validate(self, data):
+        start = data.get('start_time')
+        end = data.get('end_time')
+        if start and end and start >= end:
+            raise serializers.ValidationError({
+                'end_time': 'End time must be after start time.'
+            })
+        return data
+
     class Meta:
         model = Event
         fields = [
