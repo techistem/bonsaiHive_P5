4aff73ec9a25773f1226ac75164ea7f1d001ae80
Istem Karakas <istem.karakas@gmail.com>
2025-09-23 13:38:16 +0100

    Set DEBUG=False for Heroku deployment

--- bonsaiHive_P5/settings.py
+++ bonsaiHive_P5/settings.py
@@ -59,8 +59,12 @@ REST_AUTH_SERIALIZERS = {
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+# DEBUG = 'DEBUG' in os.environ
+# DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST', 'localhost'),
@@ -214,7 +218,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
